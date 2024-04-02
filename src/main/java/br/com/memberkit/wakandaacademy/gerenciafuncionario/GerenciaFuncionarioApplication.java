9995d7271e13f6dae665683239eb6e1e9cb2e820
LiwDev <liw.dev.23@gmail.com>
2024-04-02 20:02:39 -0300

    inicia projeto

---
+++ src/main/java/br/com/memberkit/wakandaacademy/gerenciafuncionario/GerenciaFuncionarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.memberkit.wakandaacademy.gerenciafuncionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciaFuncionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciaFuncionarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/memberkit/wakandaacademy/gerenciafuncionario/GerenciaFuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.memberkit.wakandaacademy.gerenciafuncionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciaFuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
