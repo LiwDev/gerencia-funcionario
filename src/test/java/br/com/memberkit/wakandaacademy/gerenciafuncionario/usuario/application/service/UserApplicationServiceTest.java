8476f1779b6ae10a743c65b9118be164ade6b34d
LiwDev <liw.dev.23@gmail.com>
2024-10-22 23:20:04 -0300

    :white_check_mark: criando as classes de teste de todas as classes da camada de services

--- src/test/java/br/com/memberkit/wakandaacademy/gerenciafuncionario/usuario/application/service/UserApplicationServiceTest.java
+++ src/test/java/br/com/memberkit/wakandaacademy/gerenciafuncionario/usuario/application/service/UserApplicationServiceTest.java
@@ -1,4 +1,16 @@
+package br.com.memberkit.wakandaacademy.gerenciafuncionario.usuario.application.service;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserApplicationServiceTest {
-  
+
+    @Test
+    void criaUsuario() {
+    }
+
+    @Test
+    void findByLogin() {
+    }
 }
